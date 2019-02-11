18c1226627a7d80df12fbeaff847ff969c9c954a
Nathan Thomas <nwthomas@me.com>
2019-02-11 12:43:17 -0800

    Add server in index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+
+const express = require("express");
+const db = require("./data/db");
+const server = express();
+server.use(express.json());
+
+server.listen(5000, () => {
+  console.log("\n***** Listening on Port 5000 *****\n");
+});
