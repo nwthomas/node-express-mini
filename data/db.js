809c7f493259f9012dd428c68784eb00329fbfbd
Nathan Thomas <nwthomas@me.com>
2019-02-11 13:16:35 -0800

    Make .get() and return resolve/reject conditionally

--- data/db.js
+++ data/db.js
@@ -1,39 +1,59 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile.js');
+const knex = require("knex");
+const knexConfig = require("../knexfile.js");
 const db = knex(knexConfig.development);
 
 module.exports = {
-  find,
-  findById,
-  insert,
-  update,
-  remove,
+  hubs: {
+    find,
+    findById,
+    insert,
+    update,
+    remove
+  }
 };
+let nextId = 1;
+
+const _hubs = [
+  {
+    name: "Jane Doe", // String, required
+    bio: "Not Tarzan's Wife, another Jane", // String
+    created_at: "Mon Aug 14 2017 12:50:16 GMT-0700 (PDT)", // Date, defaults to current date
+    updated_at: "Mon Aug 14 2017 12:50:16 GMT-0700 (PDT)" // Date, defaults to current date
+  }
+];
 
 function find() {
-  return db('users');
+  // return db(_hubs); // I couldn't get this to work
+  if (_hubs.length > 0) {
+    return Promise.resolve(_hubs);
+  } else {
+    return Promise.reject({
+      code: 500,
+      message: "The user information could not be retrieved."
+    });
+  }
 }
 
 function findById(id) {
-  return db('users')
+  return db("users")
     .where({ id: Number(id) })
     .first();
 }
 
 function insert(user) {
-  return db('users')
-    .insert(user)
-    .then(ids => ({ id: ids[0] }));
+  // return db("users")
+  //   .insert(user)
+  //   .then(ids => ({ id: ids[0] }));
 }
 
 function update(id, user) {
-  return db('users')
-    .where('id', Number(id))
+  return db("users")
+    .where("id", Number(id))
     .update(user);
 }
 
 function remove(id) {
-  return db('users')
-    .where('id', Number(id))
+  return db("users")
+    .where("id", Number(id))
     .del();
 }
--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const db = require("./data/db");
 const server = express();
 server.use(express.json());
 
+server.get("/api/users", (req, res) => {
+  db.hubs
+    .find()
+    .then(hubs => {
+      res.status(200).json({ success: true, hubs });
+    })
+    .catch(err => {
+      res.status(err.code).json({ success: false, message: err.message });
+    });
+});
+
+server.post("/api/users", (req, res) => {
+  const hub = req.body;
+  db.hubs
+    .insert(hub)
+    .then(hub => {
+      res.status(201).json({ success: true, hub });
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ success: false, message });
+    });
+});
+
 server.listen(5000, () => {
   console.log("\n***** Listening on Port 5000 *****\n");
 });
